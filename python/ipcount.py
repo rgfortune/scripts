4ba644e5bf0df23c0d820e0b7e2898634df106d4
Ricardo Fortune <ricardo.fortune@rackspace.co.uk>
2015-05-18 17:15:59 +0100

    ipcount.py handling stdin and cli arguments

--- python/ipcount.py
+++ python/ipcount.py
@@ -2,31 +2,42 @@
 # Version 0.1
 
 # This script parses an Apache access log file and produces a list of the IPs with the most requests.
-
-# Usage:
-#   ipcount.py /path/to/log/file
+# 
 #
-#   If you want to filter by time you could pipe stdout into ipcount.py
-#   egrep "(0|1)[0-9]/[A-Z][a-z]{2}/2015:13:4[0-7]" /path/to/log/file | ipcount.py 
+# Usage:
+#   $ ipcount.py /path/to/log/file
+#
+#   The script can also be used in a tool chain.
+#
+#   $ cat /path/to/log/file | ipcount.py  
+#   $ egrep "(0|1)[0-9]/[A-Z][a-z]{2}/2015:13:4[0-7]" /path/to/log/file | ipcount.py 
 
 import re, sys
 
-# Read in log file
-logfile = open(sys.argv[1])
 
-# RegEx to match for IPs in the log file
-ipRegEx = re.compile(r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}')
+if __name__=="__main__":
+
+  # RegEx to match for IPs in the log file
+  ipRegEx = re.compile(r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}')
 
 
-# Iterate through the log, line by line, and keep running count of IPs
-ipHash = {}
-for line in logfile.readlines():
-  mo = ipRegEx.search(line)
-  ipHash.setdefault(mo.group(), 0)
-  ipHash[mo.group()] += 1
+  # Iterate through the log, line by line, and keep running count of IPs
+
+
+  if sys.argv[1]:
+    logfile = open(sys.argv[1])
+    for line in logfile.readlines():
+      mo = ipRegEx.search(line)
+      ipHash.setdefault(mo.group(), 0)
+      ipHash[mo.group()] += 1
+  else:
+    for line in sys.stdin.readlines():
+      mo = ipRegEx.search(line)
+      ipHash.setdefault(mo.group(), 0)
+      ipHash[mo.group()] += 1
   
-# Sort and print formatted
+  # Sort and print formatted
 
-sortIpHash = sorted(ipHash, key=ipHash.__getitem__, reverse=True)
-for ip in sortIpHash:
-  print(ip + ': ' + str(ipHash[ip]))
+  sortIpHash = sorted(ipHash, key=ipHash.__getitem__, reverse=True)
+  for ip in sortIpHash:
+    print(ip + ': ' + str(ipHash[ip]))
